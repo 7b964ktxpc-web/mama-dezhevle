01c56b730548154e0714078b7a95d88d887f8ac7
7b964ktxpc-web <7b964ktxpc@privaterelay.appleid.com>
2026-08-18 13:04:01 +0700

    feat(parser): export normalization and catalog pipeline

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./types";
 export * from "./engine";
 export * from "./matching";
+export * from "./normalize";
 export * from "./price";
+export * from "./quality";
+export * from "./catalog";
